63cf3ede677b52a3c05aea20de760becc954fb34
GianmarcoBasile <basilemarc2000@gmail.com>
2026-03-28 01:59:46 +0100

    fix: chord transpose

--- chord-extractor-service/utility.py
+++ chord-extractor-service/utility.py
@@ -1,46 +1,64 @@
 from chord_extractor import ChordChange
-from typing import List, Tuple
+from typing import Dict, List, Tuple
+import re
 
-sharp_scale:List[str] = ["C","C#","D","D#","E","F","F#","G","G#","A", "A#","B"]
-flat_scale:List[str] = ["C","Db","D","Eb","E","F","Gb","G","Ab","A", "Bb","B"]
+NOTE_TO_INDEX:Dict[str,int] = {
+    "C": 0, "C#": 1, "Db": 1,
+    "D": 2, "D#": 3, "Eb": 3,
+    "E": 4,
+    "F": 5, "F#": 6, "Gb": 6,
+    "G": 7, "G#": 8, "Ab": 8,
+    "A": 9, "A#": 10, "Bb": 10,
+    "B": 11
+}
 
-def swap_chords(old_chord: str, new_computed_chord: str) -> str:
-  if len(old_chord) > 1 and (old_chord[1] == "#" or old_chord[1] == 'b'):
-    new_computed_chord = new_computed_chord + old_chord[2:]
-  else:
-    new_computed_chord = new_computed_chord + old_chord[1:]
-  return new_computed_chord
+INDEX_TO_NOTE_SHARP = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+INDEX_TO_NOTE_FLAT  = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"]
 
-def compute_transposed_chord(chord:str, value:int, scale:str) -> str:
-  parts = chord.split("/")
-  old_chord:str = parts[0]
-  bass_old_chord:str = ""
-  if len(parts) > 1:
-    bass_old_chord = parts[1]
+def split_chord(chord: str) -> Tuple[str, str]:
+    """
+    Divide l'accordo in root (es. C, F#) e suffisso (es. m7, maj7)
+    """
+    match = re.match(r"([A-G][#b]?)(.*)", chord)
+    if match:
+        return match.group(1), match.group(2)
+    return chord, ""
 
-  if scale == '#':
-    sharp_step:int = 2 if len(old_chord) > 1 and old_chord[1] == "#" else 1
-    if(len(bass_old_chord) > 0):
-      return swap_chords(old_chord, sharp_scale[(sharp_scale.index(old_chord[:sharp_step]) + value)%len(sharp_scale)]) + "/" + sharp_scale[(sharp_scale.index(bass_old_chord) + value)%len(sharp_scale)]
+def transpose_root(root: str, value: int, prefer_sharp: bool = True) -> str:
+    # Ottieni indice logico
+    idx = NOTE_TO_INDEX[root]
+    new_idx = (idx + value) % 12
+
+    # Scegli notazione finale
+    if prefer_sharp:
+        return INDEX_TO_NOTE_SHARP[new_idx]
     else:
-      return swap_chords(old_chord, sharp_scale[(sharp_scale.index(old_chord[:sharp_step]) + value)%len(sharp_scale)])
-  elif scale == 'b':
-    flat_step:int = 2 if len(old_chord) > 1 and old_chord[1] == "b" else 1
-    if(len(bass_old_chord) > 0):
-      return swap_chords(old_chord, flat_scale[(flat_scale.index(old_chord[:flat_step]) + value)%len(flat_scale)]) + "/" + flat_scale[(flat_scale.index(bass_old_chord) + value)%len(flat_scale)]
-    else:
-      return swap_chords(old_chord, flat_scale[(flat_scale.index(old_chord[:flat_step]) + value)%len(flat_scale)])
+        return INDEX_TO_NOTE_FLAT[new_idx]
 
-def transpose(chords: List[ChordChange], value:int) -> List[ChordChange]:
-  transposed_chords:List[ChordChange] = []
-  for c in chords:
-    if c.chord != "N":
-      if c.chord.find("#") != -1 or value > 0:
-        new_chord:str = compute_transposed_chord(c.chord, value, "#")
-        transposed_chords.append(ChordChange(new_chord, c.timestamp))
-      else:
-        new_chord:str = compute_transposed_chord(c.chord, value, "b")
-        transposed_chords.append(ChordChange(new_chord, c.timestamp))
+def compute_transposed_chord(chord: str, value: int, prefer_sharp: bool = True) -> str:
+    """
+    Trasposta un accordo completo, root, suffisso e basso incluso
+    """
+    parts = chord.split("/")
+    root, suffix = split_chord(parts[0])
+    new_root = transpose_root(root, value, prefer_sharp)
+
+    if len(parts) > 1:
+        bass_root, bass_suffix = split_chord(parts[1])
+        new_bass = transpose_root(bass_root, value, prefer_sharp)
+        return f"{new_root}{suffix}/{new_bass}{bass_suffix}"
     else:
-      transposed_chords.append(ChordChange(c.chord, c.timestamp))
-  return transposed_chords
+        return f"{new_root}{suffix}"
+
+def transpose(chords: List[ChordChange], value: int, prefer_sharp: bool = True) -> List[ChordChange]:
+    """
+    Trasposta una lista di ChordChange di `value` semitoni
+    """
+    transposed: List[ChordChange] = []
+    for c in chords:
+        if c.chord != "N":
+            new_chord = compute_transposed_chord(c.chord, value, prefer_sharp)
+            transposed.append(ChordChange(new_chord, c.timestamp))
+        else:
+            transposed.append(ChordChange(c.chord, c.timestamp))
+    return transposed
